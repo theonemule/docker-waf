ec8be254095bd57b40da490737c7c418c516db40
root <me@blaize.net>
2018-12-04 09:51:30 -0800

    updated ubuntu and nginx versions

--- waf/build.multi.sh
+++ waf/build.multi.sh
@@ -13,9 +13,9 @@ make
 
 #make nginx
 cd /
-wget http://nginx.org/download/nginx-1.10.2.tar.gz
-tar xvzf nginx-1.10.2.tar.gz
-cd ../nginx-1.10.2
+wget http://nginx.org/download/nginx-1.15.7.tar.gz
+tar xvzf nginx-1.15.7.tar.gz
+cd ../nginx-1.15.7
 ./configure --user=root --group=root --with-debug --with-ipv6 --with-http_ssl_module --add-module=/usr/src/modsecurity/nginx/modsecurity --with-http_ssl_module --without-http_access_module --without-http_auth_basic_module --without-http_autoindex_module --without-http_empty_gif_module --without-http_fastcgi_module --without-http_referer_module --without-http_memcached_module --without-http_scgi_module --without-http_split_clients_module --without-http_ssi_module --without-http_uwsgi_module
 make
 make install
